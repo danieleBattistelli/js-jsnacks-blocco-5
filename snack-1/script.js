c7b4b3335e526748b7b94f9970395f626bf8e823
danieleBattistelli <bomber81@gmail.com>
2024-11-15 15:50:14 +0100

    snack 1-3

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+console.log('snack-1');
+console.table(names);
+
+console.log('----------------------------------------------------------')
---
+++ snack-2/script.js
@@ -0,0 +1,11 @@
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
+console.log('snack-2');
+const peopleName = people.filter(person => person.name);
+console.table(peopleName);
